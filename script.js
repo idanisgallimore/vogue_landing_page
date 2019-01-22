b3d0168aabd86661328d563924185f5e6c325473
idanisg <idanisgallimore@gmail.com>
2019-01-21 21:03:38 -0500

    added ad container

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ var icon2 = document.querySelector(".iconnav2");
 var navText = document.querySelector(".nav-text");
 var smallTxt = document.querySelector(".smalltext");
 var brandsmall = document.querySelector(".brandsmall");
-
-
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -25,3 +23,27 @@ function scrollFunction() {
     icon2.style.border = "0";
   }
 }
+// loopImg();
+var adCont = document.querySelector(".add");
+var adSrcLinks = ['./images/ad1.jpg', "./images/ad2.jpg", "./images/ad3.png"];
+function loopColors(){
+  var i = 0;
+  setInterval(()=>{
+    adCont.style.backgroundImage = 'url("'  + adSrcLinks[i++ % adSrcLinks.length] + '")';
+  }, 10000);
+};
+loopColors();
+
+// Date stuff 
+var date = new Date();
+var month = date.getMonth(); 
+var year = date.getFullYear();
+var day = date.getDate();
+// get the day of the week 
+var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var months = ["January", "February", "March", "April", "May",
+"June", "July", "August", "September", "October", "November", "December"];
+var dayme = date.getDay();
+var fullDate = dayOfWeek[dayme] + ", " + months[month] + " " + day + ", " + year;
+var pageDay = document.querySelector(".date");
+pageDay.textContent = fullDate;
